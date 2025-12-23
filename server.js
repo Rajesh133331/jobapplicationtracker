3219202babd3c57f46931f0fc3736f9daa0fdc54
Rajesh133331 <104456124+Rajesh133331@users.noreply.github.com>
2025-12-23 15:59:56 +0530

    siva Update server.js

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ app.post("/mul", (req, res) => {
   const { num1, num2 } = req.body;
 
   // Validation
-  if (typeof num1 !== "number" || typeof num2 !== "number") {
-    return res.status(400).json({
-      error: "num1 and num2 must be numbers"
-    });
-  }
 
   const result = num1 * num2;
 
